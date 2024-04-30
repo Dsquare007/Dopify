0da17782647bfcf0d729f31bc0cd7241f90abacf
diwakar4586 <diwakar0470.be21@chitkara.eud.in>
2024-04-30 23:27:44 +0530

    rightBar-component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="w-3/4 flex justify-between h-[64px] bg-gradient-to-b from-neutral-800 to-neutral-900 text-white  ">
+      <div className="p-2 m-2 w-full flex justify-between ">
+        <div className="">
+          <div className="flex gap-[0.5rem]">
+            <div className="w-8 h-8 flex items-center justify-center bg-black rounded-full">
+              <svg
+                class="h-6 w-6 text-white text-center"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 19l-7-7 7-7"
+                />
+              </svg>
+            </div>
+            <div className="w-8 h-8 flex items-center justify-center bg-black rounded-full">
+              <svg
+                class="h-6 w-6 text-white"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                {" "}
+                <polyline points="9 18 15 12 9 6" />
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex gap-[0.5rem]">
+          
+          <button type="button" class="h-8 w-36 pb-[0.23rem] text-sm font-medium text-black focus:outline-none bg-white rounded-full border border-gray-200  hover:scale-110 transition-all ">Explore Premium</button>
+          <button type="button" class="h-8 w-28 pb-[0.23rem] text-sm font-medium text-white outline-none focus:outline-none bg-black rounded-full   hover:scale-110 transition-all ">! Install App</button>
+         
+          <div className="w-8 h-8 flex items-center justify-center bg-black rounded-full">
+          <svg class="h-6 w-6 text-gray-400"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />  <path d="M9 17v1a3 3 0 0 0 6 0v-1" /></svg>
+            </div>
+       
+            <div className="w-8 h-8 flex items-center justify-center bg-black rounded-full">
+            <div className="w-6 h-6 text-black font-bold  flex items-center justify-center bg-green-500 rounded-full">
+          D
+            </div>
+            </div>
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
